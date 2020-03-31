7f0fe1e3ba3bbfdaebfae9ab2adb389edad3a836
Apoorv Goyal <apoorvelous@gmail.com>
2020-03-31 15:36:09 +0530

    blog added

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template("/index.html")
 
+@app.route('/blog')
+def blog():
+    return render_template("/blog.html")
+
 if __name__ == "__main__":
     app.run()
